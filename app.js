ad6cbd1a9bca6ad25f19f06a76b2ff45d2304e8d
Hector Milla <37377168+HectorMilla@users.noreply.github.com>
2023-10-24 21:46:15 -0400

    added button to delete book item as well as change read status; added basic css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const bookList = document.getElementById("books");
+const bookList = document.querySelector("#books");
 const newBookButton = document.getElementById("addBook");
 const bookForm = document.querySelector("#bookForm");
 
@@ -11,13 +11,20 @@ function Book(title, author, pages, read) {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
   };
 }
-myLibrary = ["The Hobbit", "Eragon"];
+myLibrary = ["The Hobbit", "Eragon", "test1", "test2"];
 //loops through array of books and displays them on screen
-let addBookToLibrary = () => {
-  myLibrary.forEach((book) => {
-    const libraryBook = document.createElement("div");
-    libraryBook.innerText = book;
-    bookList.appendChild(libraryBook);
+
+let displayBooks = (library) => {
+  library.forEach((book, index) => {
+    const bookHtml = `
+    <div id=${index} class=book>
+        ${book}
+        <div>
+            <button data-index=${index} class=deleteBook>Delete</button>
+            <button data-index=${index} class=readStatus>Mark as read</button>
+        </div>
+    </div>`;
+    bookList.innerHTML += bookHtml;
   });
 };
 
@@ -25,10 +32,27 @@ newBookButton.addEventListener("click", () => {
   bookForm.style.display = "block";
 });
 //prevent form submit default and remove form from screen
+displayBooks(myLibrary);
 document.getElementById("submit").addEventListener("click", function (event) {
   event.preventDefault();
   bookForm.style.display = "none";
 });
-addBookToLibrary();
+
+const deleteBookButton = document.querySelectorAll(".deleteBook");
+
+for (let i = 0; i < deleteBookButton.length; i++) {}
+
+bookList.addEventListener("click", (e) => {
+  let elementId = e.target.dataset.index;
+  let elementClass = e.target.className;
+  let element = document.getElementById(elementId); // book div
+  if (elementClass == "deleteBook") {
+    myLibrary.splice(elementId, 1);
+    element.remove();
+  } else if (elementClass == "readStatus") {
+    element.classList.toggle("read");
+  }
+});
+let markAsRead = () => {};
 // let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "read");
 // console.log(theHobbit.info());
